93ede2eb426f57f9845d34216a9ba4e03c36ed49
Cyril David <cyx.ucron@gmail.com>
2009-06-03 16:17:40 +0800

    fix for TwitterFu.updates

--- lib/twitter_fu.rb
+++ lib/twitter_fu.rb
@@ -3,8 +3,8 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__)))
 module TwitterFu
   autoload :Client, 'twitter_fu/client'
   
-  def self.updates( username )
-    Client.new( username ).updates
+  def self.updates( username, *args )
+    Client.new( username ).updates( *args )
   end
   
   def self.update( username, since = Time.now.utc )
